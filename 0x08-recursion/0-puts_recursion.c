02562346c7caec635908115e11009b7ae9938761
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2023-10-25 07:52:12 -0700

    edited the file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 void _puts_recursion(char *s){
 	if (*s == '\0'){
-		putchar('\n');
+		_putchar('\n');
 	}
 
-	putchar(*s);
+	_putchar(*s);
 	_puts_recursion(s+1);
 }
 
-int main(){
-	char str[] = "Puts with recursion";
-	_puts_recursion(str);
-	return 0;
-}
