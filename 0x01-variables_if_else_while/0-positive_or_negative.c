ac7e88abc8d0c9c6efba23a82c3b9102121294d0
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-03-08 21:40:03 -0800

    cor

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/** This program determines if a randomly generated 
+/** This program determines if a randomly generated
  * number is positive or negative
  */
 int main(void)
