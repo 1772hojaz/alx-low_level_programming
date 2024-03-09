a5546cdf901a6dad321fbeb99138b3528366b677
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-03-08 21:43:25 -0800

    correction of sed -i 's/[[:space:]]*$//' 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    char m;
+	char m;
 
-    for (m = 'a'; m <= 'z'; m++)
-    {
-        putchar(m);
-    }
-    putchar('\n');
-    return 0;
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
 }
