7e759f08298da30cab37a4b671b75e0e6b99ddc5
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-03-13 11:02:53 -0700

    added ./8-24 | head

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * jack_bauer - Entry point
+ * @r: last parameter
+ * @x: first parameter
+ * @y: second parameter
+ * @z: third parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int r;
+	int x;
+	int y;
+	int z = 0;
+
+	for (x = 0; x <= 2; x++)
+	{
+
+		for (y = 0; y <= 3; y++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				for (r = 0; r <= 9; r++)
+				{
+					_putchar(x + '0');
+					_putchar(y + '0');
+					_putchar(':');
+					_putchar(z + '0');
+					_putchar(r + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
