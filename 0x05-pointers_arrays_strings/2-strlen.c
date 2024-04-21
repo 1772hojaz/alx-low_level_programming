83da8884d69142b3009af52c6f7c3e0a081e0551
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-04-21 15:10:59 -0700

    added file2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strlen - main entry
+ *
+ * @s: pointer to the string
+ * @n: the length of the string
+ * Return: Always 0(Success)
+ */
+int _strlen(char *s);
+int _strlen(char *s)
+{
+	return strlen(s);
+}
