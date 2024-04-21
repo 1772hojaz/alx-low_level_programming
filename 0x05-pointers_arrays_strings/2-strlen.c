30f619acd404f3060f750569dd79f654ceaf153e
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-04-21 15:14:34 -0700

    corrected file 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,11 +3,11 @@
  * _strlen - main entry
  *
  * @s: pointer to the string
- * @n: the length of the string
+ * @n: length of the string
  * Return: Always 0(Success)
  */
 int _strlen(char *s);
 int _strlen(char *s)
 {
-	return strlen(s);
+	return (strlen(s));
 }
