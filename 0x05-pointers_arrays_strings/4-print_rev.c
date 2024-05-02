425a023b9059519a0966bff0a729652611be6dd7
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-05-02 15:32:44 -0700

    added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: is a pointer to the sting
+ */
+void print_rev(char *s);
+void print_rev(char *s)
+{
+	int i;
+	int len;
+
+	len = strlen(s);
+        for (i=len-1; i>=0; i--)
+        {
+                _putchar(s[i]);
+        }
+        _putchar('\n');
+}
