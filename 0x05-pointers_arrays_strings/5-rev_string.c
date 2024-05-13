a1f80b7d791d86fe6da2535bb2720a47cc1d6e40
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-05-12 20:13:13 -0700

    added coments to file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,20 @@
 void rev_string(char *s);
 void rev_string(char *s)
 {
+	 /* length of the string */
 	int len = strlen(s);
+	/* loop variable and temporary variable for swapping */
 	int i;
 	char temp;
 
+	 /* Iterate through the first half of the string */
 	for (i = 0; i < len / 2; i++)
 	{
+		 /* Swap characters: store the character at position i in temp */
 		temp = s[i];
+		/* Replace character at position i with the character at position len - 1 - i */
 		s[i] = s[len - 1 - i];
+		/* Replace character at position len - 1 - i with the character stored in temp */
 		s[len - 1 - i] = temp;
 	}
 }
