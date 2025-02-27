addd0d630595f9651602cb352a84d82660da7059
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-02-27 18:45:26 +0200

    I ADDED #0x04-more_functions_nested_loops/2-main.c,#0x04-more_functions_nested_loops/2-mul #0x04-more_functions_nested_loops/2-mul.c, UPDATED #0x04-more_functions_nested_loops/main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies two integers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
