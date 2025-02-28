af5f96fea697ac726c2e5c9e99e7201ccb279be8
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-02-28 08:57:59 +0200

    I, UPDATED #0x04-more_functions_nested_loops/5-more_numbers,#0x04-more_functions_nested_loops/5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,25 +3,26 @@
 /**
  * more_numbers - prints 10 times the numbers from 0 to 14
  *
- * Return: Always(0)
+ * Return: Always (0).
  */
 
 void more_numbers(void)
 {
 	int i = 10;
 	int j = 14;
+
 	int x;
 	int y;
 
 	for (x = 0; x < i; x++)
 	{
-		for (y = 0; y <= j; y++)
+		for  (y = 0; y <= j; y++)
 		{
-			if (y > 10)
+			if (y >= 10)
 			{
-				_putchar(1 + '0');
+				_putchar('1');
 			}
-			_putchar(y % 10  + '0');
+			_putchar(y % 10 + '0');
 		}
 		_putchar('\n');
 	}
