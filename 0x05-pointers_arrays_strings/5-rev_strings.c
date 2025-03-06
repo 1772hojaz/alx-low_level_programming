c3b74c9f7bc905e275f1cc43e9105c8ae94936b7
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-03-06 20:19:36 +0200

    I ADDED #0x05-pointers_arrays_strings/5-rev_strings.c

---
+++ 0x05-pointers_arrays_strings/5-rev_strings.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string
+ * using pointers
+ *
+ * @s: string
+ * @a: string
+ * @b: string
+ * @*s: a poiter to the string s
+ * @*a: start
+ * @*b: stop
+ *
+ * Return: Nothinng
+ */
+
+void rev_string(char *s)
+{
+	char *a = s;
+	char *b = s;
+
+
+}
