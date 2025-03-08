1f1bb8dfccbfd52c3f0f2849f7cba3a4299a4c70
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-03-08 08:01:24 +0200

    I ADDED #0x05-pointers_arrays_strings/100-atoi,#0x05-pointers_arrays_strings/100-atoi.c #0x05-pointers_arrays_strings/100-main.c, UPDATED #0x05-pointers_arrays_strings/main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stddef.h>
 
 void rev_string(char *s);
 int _putchar(char c);
@@ -14,4 +15,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif
